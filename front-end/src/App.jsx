f182af6e379f92eacf3212b5b0b3efc49973b26e
saksham <22bcs094@nith.ac.in>
2024-03-09 05:08:54 +0530

    chatbot

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,20 +1,91 @@
-import React from 'react';
-import { ReactDOM } from 'react';
+import React, { useState } from 'react';
+import './style.css'; // Assuming you have a corresponding style.css file
 
+function Chatbot() {
+    const [messages, setMessages] = useState([]);
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const date = new Date();
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        const str_time = hour + ":" + minute;
+        const rawText = event.target.elements.msg.value;
 
-import Login from './components/Login';
+        const userHtml = (
+            <div className="d-flex justify-content-end mb-4">
+                <div className="msg_cotainer_send">
+                    {rawText}
+                    <span className="msg_time_send">{str_time}</span>
+                </div>
+                <div className="img_cont_msg">
+                    <img src="https://i.ibb.co/d5b84Xw/Untitled-design.png" className="rounded-circle user_img_msg" alt="user" />
+                </div>
+            </div>
+        );
 
-  function App() {
+        setMessages([...messages, userHtml]);
+
+        const response = await fetch('/get', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ msg: rawText })
+        });
+        const data = await response.json();
+
+        const botHtml = (
+            <div className="d-flex justify-content-start mb-4">
+                <div className="img_cont_msg">
+                    <img src="https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png" className="rounded-circle user_img_msg" alt="bot" />
+                </div>
+                <div className="msg_cotainer">
+                    {data}
+                    <span className="msg_time">{str_time}</span>
+                </div>
+            </div>
+        );
+
+        setMessages([...messages, botHtml]);
+        event.target.elements.msg.value = ''; // Clear input field after submit
+    };
+
     return (
-      <div>
-      <h1>App</h1>
-      <Login />
-      </div>
-        
+        <div className="container-fluid h-100">
+            <div className="row justify-content-center h-100">
+                <div className="col-md-8 col-xl-6 chat">
+                    <div className="card">
+                        <div className="card-header msg_head">
+                            <div className="d-flex bd-highlight">
+                                <div className="img_cont">
+                                    <img src="https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png" className="rounded-circle user_img" alt="chatbot" />
+                                    <span className="online_icon"></span>
+                                </div>
+                                <div className="user_info">
+                                    <span>ChatBot</span>
+                                    <p>Ask me anything!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="messageFormeight" className="card-body msg_card_body">
+                            {messages.map((message, index) => (
+                                <React.Fragment key={index}>{message}</React.Fragment>
+                            ))}
+                        </div>
+                        <div className="card-footer">
+                            <form onSubmit={handleSubmit} className="input-group" id="messageArea">
+                                <input type="text" id="text" name="msg" placeholder="Type your message..." autoComplete="off" className="form-control type_msg" required />
+                                <div className="input-group-append">
+                                    <button type="submit" id="send" className="input-group-text send_btn"><i className="fas fa-location-arrow"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
-    )
-  }
-
-
-export default App;
+export default Chatbot;
