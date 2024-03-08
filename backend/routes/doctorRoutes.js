283da5fee4223e14b0ee56063ffc885c0cd68eb0
Sourabh-awasthy <sourabhawasthy176022@gmail.com>
2024-03-08 17:47:11 +0530

    update

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -3,26 +3,15 @@ const doctorController = require('../controllers/doctorController')
 
 const router = express.Router();
 
-<<<<<<< HEAD
-// router.route('/login').post(doctorController.login);
-
-
-// router.route('/register').post(doctorController.register);
-=======
 router.route('/login').post(doctorController.login);
 
 
 router.route('/register').post(doctorController.register);
->>>>>>> a8db449dbd0ce382844d5349cc45dfbec14033c8
 
 
 // router.route('/getDoctor').get(doctorController.getHospital);
 
 // router.route('/getPatients').get(doctorController.getDoctors);
 
-<<<<<<< HEAD
-module.exports = router
-=======
->>>>>>> a8db449dbd0ce382844d5349cc45dfbec14033c8
 
 module.exports = router
--- backend/routes/hospitalRoutes.js
+++ backend/routes/hospitalRoutes.js
@@ -13,11 +13,5 @@ router.route('/getHospital').get(getAllHospitals);
 
 router.route('/getDoctors').get(getAlldoctors);
 
-<<<<<<< HEAD
-// router.route('/getDoctors').get(hospitalController.getDoctors);
 
 module.exports = router;
-=======
-
-module.exports = router;
->>>>>>> a8db449dbd0ce382844d5349cc45dfbec14033c8
