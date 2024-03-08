f05a6948548aa5c1dffd8f20b579878356ab7329
Saksham29-2004 <147153342+Saksham29-2004@users.noreply.github.com>
2024-03-09 04:47:15 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, jsonify
+
+import os
+import openai
+import langchain
+from langchain_openai import OpenAI
+os.environ['OPENAI_API_KEY'] = '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'
+llm = OpenAI(temperature=0.0)
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return get_Chat_response(input)
+
+
+def get_Chat_response(text):
+
+    for step in range(5):
+        response = llm.invoke(text) 
+        return response
+
+
+if __name__ == '__main__':
+    app.run()
+
+def new1():
+    return None
