7158d2a5a5f23cb85f637b198a987853504e1e59
Sourabh-awasthy <sourabhawasthy176022@gmail.com>
2024-03-08 16:51:18 +0530

    frontend set up

---
+++ front-end/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+
+
+
+import Login from './components/Login';
+
+  function App() {
+    return (
+      <>
+      <h1>App</h1>
+      <Login />
+      </>
+        
+
+    )
+  }
+
+
+export default App;
