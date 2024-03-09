c80dc3c63d207b9eca745ac0c233f9720e5cb08d
mehulambastha <mehul.213amb@gmail.com>
2024-03-10 02:40:56 +0530

    linked sidebar

--- front-end/src/components/dashboard/components/Sidebar.jsx
+++ front-end/src/components/dashboard/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({changeViewingMode}) => {
   return (
     <div className="">
       <h1 className="flex text-4xl py-10 items-center justify-center gap-2 text-white">
@@ -8,19 +8,19 @@ const Sidebar = () => {
         Nirog
       </h1>
       <div className="flex flex-col gap-2 text-xl w-full px-5 text-white">
-        <p className="flex items-center justify-center">
+        <p className="flex items-center justify-center cursor-pointer" onClick={()=>changeViewingMode('landing')}>
           <img className="inline" width={80} src="/dbb.png"/>
           Dashboard
         </p>
-        <p className="flex items-center justify-center">
+        <p className="flex items-center justify-center cursor-pointer" onClick={()=>changeViewingMode('report')}>
           <img className="inline" width={80} src="/reports.png"/>
           My Reports
         </p>
-        <p className="flex items-center justify-center">
+        <p className="flex items-center justify-center cursor-pointer" onClick={()=>changeViewingMode('prescription')}>
           <img className="inline" width={80} src="/prescription.png"/>
           My Prescriptions
         </p>
-        <p className="flex items-center justify-center">
+        <p className="flex items-center justify-center cursor-pointer" onClick={()=>changeViewingMode('doxaab')}>
           <img className="inline" width={80} src="/doxxab.png"/>
           DOXAAB (beta)
         </p>
