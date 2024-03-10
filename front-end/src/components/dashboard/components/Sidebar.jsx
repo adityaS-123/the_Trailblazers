5b56d8a87a1c764fc6fa3e3c488f3a6f802035ca
saksham <22bcs094@nith.ac.in>
2024-03-10 08:41:35 +0530

    adds system

--- front-end/src/components/dashboard/components/Sidebar.jsx
+++ front-end/src/components/dashboard/components/Sidebar.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 const Sidebar = ({ changeViewingMode }) => {
   return (
     <div> {/* Reduce the width by 30% */}
-<<<<<<< HEAD
-      <h1 className="flex text-4xl mt-[-39px] py-20 items-center justify-center gap-2 text-white font-mono">
-=======
       <h1 className="flex text-2xl pt-12 items-center justify-center gap-2 text-white font-mono">
->>>>>>> 05c6d8a9c24e41d670652677def48f3d4806e043
         <img src="/medico-white.png" width={80} alt="Medico logo"/>
         NIROG
       </h1>
